15ae0011a4dbc42f799015c1b83f7b468506db72
Mahmoud <mahmoudmarey226>
2022-11-26 19:56:21 +0200

    State implemented and tested

--- src/main/java/State.java
+++ src/main/java/State.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 
 /*
+
 |o|o|o|o|o|o|Y|
 |o|o|o|o|o|o|Y|
 |o|o|o|o|Y|o|Y|
@@ -15,6 +16,7 @@ sign    height, garbage , arrangement
 
 g is garbage
 
+
 the arrangement bits are read from right to left (down to top in grid)
 
 0 is 'Y'     and 1 is 'R'
@@ -26,13 +28,13 @@ public class State {
     private static final int ROWS = 6;
     private static final int COLUMNS = 7;
     // columns masks , 511 is nine ones
-    private static final long col1Mask = 0b111111111L << (9*0);//55 zeros 9 ones 0 zeros
-    private static final long col2Mask = 0b111111111L << (9*1);//46 zeros 9 ones 9 zeros
-    private static final long col3Mask = 0b111111111L << (9*2);//37 zeros 9 ones 18 zeros
+    private static final long col7Mask = 0b111111111L << (9*0);//55 zeros 9 ones 0 zeros
+    private static final long col6Mask = 0b111111111L << (9*1);//46 zeros 9 ones 9 zeros
+    private static final long col5Mask = 0b111111111L << (9*2);//37 zeros 9 ones 18 zeros
     private static final long col4Mask = 0b111111111L << (9*3);//28 zeros 9 ones 27 zeros
-    private static final long col5Mask = 0b111111111L << (9*4);//19 zeros 9 ones 36 zeros
-    private static final long col6Mask = 0b111111111L << (9*5);//10 zeros 9 ones 45 zeros
-    private static final long col7Mask = 0b111111111L << (9*6);// 1 zero  9 ones 54 zeros
+    private static final long col3Mask = 0b111111111L << (9*4);//19 zeros 9 ones 36 zeros
+    private static final long col2Mask = 0b111111111L << (9*5);//10 zeros 9 ones 45 zeros
+    private static final long col1Mask = 0b111111111L << (9*6);// 1 zero  9 ones 54 zeros
     private static final long[] colsMasks = {col1Mask , col2Mask , col3Mask , col4Mask , col5Mask , col6Mask , col7Mask};
 
     public State(long state){
@@ -56,18 +58,27 @@ public class State {
         for (int col = 0; col < COLUMNS; col++) {
             long isolatedState = isolateState(col);
             long highestFullHeight = isolatedState >> 6;
+            if (highestFullHeight == 0 ){//corner case
+                continue;
+            }
             long colArrangementBits = getColArrangementBits(isolatedState , highestFullHeight);
             String colArrangementBitsS = Long.toBinaryString(colArrangementBits);
 
-            for (int i = 0; i < colArrangementBitsS.length(); i++) {
-                char bit = colArrangementBitsS.charAt(i);
-                int row = (ROWS-1) -i;
+            int i;
+            int row;
+            for (i = 0; i < colArrangementBitsS.length(); i++) {
+                char bit = colArrangementBitsS.charAt((colArrangementBitsS.length() -1) - i);
+                row = (ROWS-1) -i;
                 if (bit == '1'){
                     grid[row][col] = 'R';
                 }
                 else{
                     grid[row][col] = 'Y';
                 }
+            }
+            for(; i<highestFullHeight ; i++){
+                row = (ROWS-1) -i;
+                grid[row][col] = 'Y';
             }
 
 
@@ -78,45 +89,72 @@ public class State {
     }
 
     public static long toLong(char[][] grid){
-
-
-        return 0;
-    }
-
-    //private
-    public void initializeGrid(char[][] grid) {
-        for (int i = 0; i < ROWS; i++) {
-            for (int j = 0; j < COLUMNS; j++) {
-                grid[i][j] = 'o';
+        int[] cols = new int[7];
+        for (int col = 0; col < COLUMNS; col++) {
+            int colHeight = getColHeight(grid , col);
+            int arrangement = 0;
+            for (int row = ROWS-1 , i=0; i < colHeight; row-- , i++) {
+                int bit = (grid[row][col] == 'R') ? 1 : 0 ;
+                arrangement += bit << i;
             }
+            cols[col] = (colHeight << 6) + arrangement;
         }
+
+        long result = 0;
+        for (int col = 0; col < COLUMNS; col++) {
+            int shift = (9*((COLUMNS-1) - col));
+            result += (long)(cols[col]) << shift;
+        }
+
+        return result;
     }
 
     // check if the game finished or not
+
     public boolean isFinish(){
         return state < 0;
     }
-
     //returns arrayList of all possible neighbours
+
     public ArrayList<State> getAllNeighbours(boolean turn){
-        return null;
+        ArrayList<State> neighbours = new ArrayList<>();
+        for (int col = 0; col < COLUMNS; col++) {
+            long neighbor = addDisk(turn , col+1);
+            if (neighbor != 0L){
+                neighbours.add(new State(neighbor));
+            }
+        }
+        return neighbours;
+    }
+
+    public boolean addMove(boolean turn , int colNum){
+        long result = addDisk(turn , colNum);
+        if (result == 0L){
+            return false;
+        }
+        else{
+            this.state = result;
+            return true;
+        }
+
     }
 
     //add disk to a specific column
     //colNum from 1 to 7
-    public boolean addDisk(boolean turn , int colNum){
+    //col#1 is the most left column in grid or long
+    private long addDisk(boolean turn , int colNum){
         colNum--;
         long isolatedState = isolateState(colNum);
         long highestFullHeight = isolatedState >> 6;// potential error , -+1
 
         if (highestFullHeight == 7){
-            return false;
+            return 0;
         }
         else{
 
             //changing the arrangement in the isolated state
             long diskMask ;
-            diskMask = 1L << (highestFullHeight+1);
+            diskMask = 1L << (highestFullHeight);
             if (turn){
                 isolatedState = isolatedState | diskMask;
             }
@@ -134,12 +172,13 @@ public class State {
             isolatedState = isolatedState | highestFullHeightSetMask; //set
 
 
-            //assign the  isolated state to its place in this.state
-            isolatedState = isolatedState << (9*colNum); // return isolated state to its true place
-            this.state = this.state & (~colsMasks[colNum]);//clear the 9 bits of the column
-            this.state = this.state | isolatedState; // set the new 9 bits of the column
+            //assign the  isolated state to its place in resultState
+            long resultState = this.state;
+            isolatedState = isolatedState << (9*((COLUMNS-1)-colNum)); // return isolated state to its true place
+            resultState = resultState & (~colsMasks[colNum]);//clear the 9 bits of the column
+            resultState = resultState | isolatedState; // set the new 9 bits of the column
 
-            return true;
+            return resultState;
         }
 
 
@@ -148,26 +187,44 @@ public class State {
     //returns the col arrangement bits isolated and shifted
     //private
     public long getColArrangementBits(long isolatedState, long highestFullHeight) {
-        long mask = 0b000111111;
+        long mask = 0b000111111L;
         isolatedState = isolatedState & mask;
-        isolatedState = isolatedState >> (6-highestFullHeight);
+
+        long mask2 = (1L<<(highestFullHeight)) - 1;
+        isolatedState = isolatedState & mask2;
         return isolatedState;
     }
 
     //private
+    //takes numbers from 0 to COLUMNS-1
+    //0 is the most left col in grid and the most left 9bits in long
     public long isolateState(int colNum) {
         long state = this.state & colsMasks[colNum];
+        colNum = (COLUMNS-1)  - colNum;
         state = state >> (9*colNum);
         long clearMask = 0b111111111;
         state = state & clearMask; //clearing other bits
         return state;
     }
-
     //private
 
+    public static int getColHeight(char[][] grid, int col) {
 
+        for (int row = ROWS-1 ; row >= 0; row--) {
+            if (grid[row][col] == 'o'){
+                return (ROWS-1) - row;
+            }
+        }
+        return 6;
+    }
 
-
+    private void initializeGrid(char[][] grid) {
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLUMNS; j++) {
+                grid[i][j] = 'o';
+            }
+        }
+    }
 
 
 
